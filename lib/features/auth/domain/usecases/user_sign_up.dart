37f4d6d33c8eed681880521c0e7a04128bfdbfba
Junaidchohane <junaidchohanedu@gmail.com>
2026-01-20 16:08:14 +0500

    SignUp UseCase

---
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:blog_app/features/auth/domain/repository/repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+import 'package:blog_app/core/error/failures.dart';
+import 'package:blog_app/core/usecase/usecase.dart';
+
+class UserSignUp implements UseCase<String, UserSignUpParams> {
+  final AuthRepository authrepository;
+  UserSignUp(this.authrepository);
+  @override
+  Future<Either<Failures, String>> call(UserSignUpParams params) async {
+    return authrepository.signUpWithEmailPassword(
+      name: params.name,
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+// We create a gnaric type of usecase here bcz we just pass one parameter in genaric but we need three so we that like this
+class UserSignUpParams {
+  final String email;
+  final String password;
+  final String name;
+
+  UserSignUpParams({
+    required this.email,
+    required this.password,
+    required this.name,
+  });
+}
