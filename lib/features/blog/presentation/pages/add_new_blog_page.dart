75ce1f9312b1f03aff3287e4b79b0e8aa8420582
Junaidchohane <junaidchohanedu@gmail.com>
2026-01-22 14:26:25 +0500

    Add New Blog Page UI

--- lib/features/blog/presentation/pages/add_new_blog_page.dart
+++ lib/features/blog/presentation/pages/add_new_blog_page.dart
@@ -1,3 +1,6 @@
+import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:blog_app/features/blog/presentation/widgets/blog_editor.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class AddNewBlogPage extends StatefulWidget {
@@ -9,11 +12,89 @@ class AddNewBlogPage extends StatefulWidget {
 }
 
 class _AddNewBlogPageState extends State<AddNewBlogPage> {
+  final titleController = TextEditingController();
+  final contentController = TextEditingController();
+  List<String> selectedTopics = [];
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    contentController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.done_rounded))],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              DottedBorder(
+                options: CircularDottedBorderOptions(
+                  color: AppPallete.borderColor,
+                  dashPattern: [10, 4],
+                  strokeCap: StrokeCap.round,
+                  borderPadding: EdgeInsets.all(10),
+                ),
+                child: Container(
+                  height: 150,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.folder_open, size: 40),
+                      SizedBox(height: 15),
+                      Text("Select your image", style: TextStyle(fontSize: 15)),
+                    ],
+                  ),
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: ['Tech', 'Busi', 'Prog', 'Entertainment']
+                      .map(
+                        (e) => Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: GestureDetector(
+                            onTap: () {
+                              if (selectedTopics.contains(e)) {
+                                selectedTopics.remove(e);
+                              } else {
+                                selectedTopics.add(e);
+                              }
+
+                              setState(() {});
+                            },
+                            child: Chip(
+                              label: Text(e),
+                              color: selectedTopics.contains(e)
+                                  ? WidgetStateProperty.all(
+                                      AppPallete.gradient1,
+                                    )
+                                  : null,
+                              side: selectedTopics.contains(e)
+                                  ? null
+                                  : BorderSide(color: AppPallete.borderColor),
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+              SizedBox(height: 10),
+              BlogEditor(hintText: "Blog title", controller: titleController),
+              SizedBox(height: 10),
+              BlogEditor(hintText: "Blog title", controller: contentController),
+            ],
+          ),
+        ),
       ),
     );
   }
